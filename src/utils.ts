554832709c7c3a38e60cac8efb25d450b720512a
Tiego <tiego.torelli@autoavaliar.com.br>
2024-02-08 16:05:42 -0300

    Utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,25 @@
 const startsWithIgnoreCase = (str, prefix) => str.toLowerCase().startsWith(prefix.toLowerCase());
 
-export { startsWithIgnoreCase };
+import dotenv from "dotenv";
+const fs = require('fs');
+
+const reloadEnv = (key: string = '') => {
+    const keys = dotenv.parse(fs.readFileSync('.env'));
+    if (key) {
+        return keys[key];
+    }
+    for (const i in keys) {
+        process.env[i] = keys[i];
+    }
+};
+
+const loadPrePrompt = (): string => {
+    let prePrompt = process.env.PRE_PROMPT || '';
+    let file = 'pre-prompt.txt';
+    if (!prePrompt && fs.existsSync(file)) {
+        prePrompt = fs.readFileSync(file, 'utf8');
+    }
+    return prePrompt;
+};
+
+export { startsWithIgnoreCase, loadPrePrompt };
